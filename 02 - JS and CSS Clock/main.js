e0dbd593850ec0a6b9c54bec0e43f6ce58856973
Amin Mallol <aminm-34@hotmail.fr>
2022-04-07 19:31:52 +0100

    Done with the clock. First time doing it, was interesting. Didn't expect to be that simple. Just implies working with the transform / rotate css property, and translate divide 60 secs / 60 minutes /  12 hours into degres. Didnt' manage to prevent the hands to roll back to 90 degres when degres = 444. But kept it t hat way I i find it original. Also changed backgroud to make it a bit more cool.

---
+++ 02 - JS and CSS Clock/main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const secondHand = document.querySelector(".second-hand")
+const minuteHand = document.querySelector(".min-hand")
+const hourHand = document.querySelector(".hour-hand")
+
+function clockRun() {
+  const now = new Date()
+
+  const seconds = now.getSeconds();
+  const secondsDegre = ((seconds / 60) * 360) + 90;
+  secondHand.style.transform = `rotate(${secondsDegre}deg)`;
+
+  const minutes = now.getMinutes()
+  const minutesDegre = now.getMinutes((minutes / 60) * 360) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegre}deg)`;
+
+  const hours = now.getHours()
+  const hoursDegre = ((hours / 12) * 360) + 90;
+  hourHand.style.transform = `rotate(${hoursDegre}deg)`
+
+  console.log(`${hours}:${minutes}:${seconds}`)
+}
+
+setInterval(clockRun, 1000);
+
+/*
+  ? Readmi:
+  - set inteval
+  - style property
+  - time calculation in milliseconds
+*/
