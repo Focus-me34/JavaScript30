2835cc0eea61b3c9707339d362169dd752c4aa43
Amin Mallol <aminm-34@hotmail.fr>
2022-04-11 22:30:14 +0100

    Extremely difficult. Had a massive problem because of using an attribute 'checked true / false' Stuck during 4 hours, because wanted to do it properly, not as Wes Bos did in the challenge (His solution is actually not working). This solution is working perfectly, 100% JS and fully functional. No bug. Very happy with it. Will definitly find a way to implement this feature in the future, explaining the time invested on it. All the documentation of the code is in the main.js file

---
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const inputs = document.querySelectorAll(".inbox input[type='checkbox']");
+const inputsArr = Array.from(inputs) // ? We have to create array from inputs variable (It's a nodelist so can't access via index in for loop below)
+
+let inputStart, inputEnd;
+let clickCount = 0;    // ? 1 = inputStart, 2 = inputend. This count is the tool we use to assign elements to the correct variables
+
+// ! When clicking on a checkbox ( has attribute "type") and shift key is being pressed, we save the begin / end input selection
+document.addEventListener("click", (e) => {
+  if ((e.target.attributes.hasOwnProperty("type")) && (e.shiftKey === true)) {
+    clickCount === 0 ? inputStart = e.target : inputEnd = e.target;
+    console.log(inputStart, inputEnd);
+    clickCount++
+
+    if (clickCount === 2) {
+      // TODO: We need to call the checkElement function here, only when the user is clicking shift, and clicked 2 times:
+      // TODO: Once to setup the inputStart variable, and a second time to setup the inputEnd variable
+      checkElements();
+    }
+  }
+})
+
+// ! When clicking manually on the checkbox to UNTICK them, we need to change the "checked" property of element to false
+// ! otherwise checkElements function won't work properly (set checked but never unchecked even when clicking without the event below)
+inputs.forEach(input => {
+  input.addEventListener("click", (e) => e.target.checked ? e.target.checked = true : e.target.checked = false)
+});
+
+// ! We check the index of our elements in variables inputStart/inputEnd
+// ! If the index of start is lower than index end, it means the iterator needs to increse by one until reaching the inputEnd index
+// ! If the index of start is higher than index end, it means the iterator needs to decrease by one until reaching the inputEnd index
+// ! While looping, if the elements in between the start and the end of the loop are checked, we skip them directly. No time wasted
+// ! We reinitialise the click count to 0 zhenever we selected the start and end inputs.
+function checkElements() {
+  if (inputsArr.indexOf(inputStart) < inputsArr.indexOf(inputEnd)) {
+    for (let el = inputsArr.indexOf(inputStart); el !== inputsArr.indexOf(inputEnd); el++) {
+      console.log(inputsArr[el].checked)
+      if (inputsArr[el].checked) continue // ? Skip inputs already checked
+      inputsArr[el].checked = true
+    }
+  } else if ((inputsArr.indexOf(inputStart) > inputsArr.indexOf(inputEnd))) {
+    for (let el = inputsArr.indexOf(inputStart); el !== inputsArr.indexOf(inputEnd); el--) {
+      if (inputsArr[el].checked) continue // ? Skip inputs already checked
+      inputsArr[el].checked = true
+    }
+  }
+  clickCount = 0; // ? Reinitialise the click counter, so we can use the feature again and again
+}
