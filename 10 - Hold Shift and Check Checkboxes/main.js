0cbeeb1d70deb045e64017a8f06abf0def598a7f
Amin Mallol <aminm-34@hotmail.fr>
2022-04-11 22:40:49 +0100

    Did few changes to reduce the code base

--- 10 - Hold Shift and Check Checkboxes/main.js
+++ 10 - Hold Shift and Check Checkboxes/main.js
@@ -30,20 +30,29 @@ inputs.forEach(input => {
 // ! We check the index of our elements in variables inputStart/inputEnd
 // ! If the index of start is lower than index end, it means the iterator needs to increse by one until reaching the inputEnd index
 // ! If the index of start is higher than index end, it means the iterator needs to decrease by one until reaching the inputEnd index
-// ! While looping, if the elements in between the start and the end of the loop are checked, we skip them directly. No time wasted
-// ! We reinitialise the click count to 0 zhenever we selected the start and end inputs.
+// ! We reinitialise the click count to 0 whenever we selected the start and end inputs.
 function checkElements() {
-  if (inputsArr.indexOf(inputStart) < inputsArr.indexOf(inputEnd)) {
-    for (let el = inputsArr.indexOf(inputStart); el !== inputsArr.indexOf(inputEnd); el++) {
-      console.log(inputsArr[el].checked)
-      if (inputsArr[el].checked) continue // ? Skip inputs already checked
-      inputsArr[el].checked = true
-    }
-  } else if ((inputsArr.indexOf(inputStart) > inputsArr.indexOf(inputEnd))) {
-    for (let el = inputsArr.indexOf(inputStart); el !== inputsArr.indexOf(inputEnd); el--) {
-      if (inputsArr[el].checked) continue // ? Skip inputs already checked
-      inputsArr[el].checked = true
-    }
+  for (let el = inputsArr.indexOf(inputStart); el !== inputsArr.indexOf(inputEnd); inputsArr.indexOf(inputStart) < inputsArr.indexOf(inputEnd) ? el++ : el--) {
+    inputsArr[el].checked = true
   }
   clickCount = 0; // ? Reinitialise the click counter, so we can use the feature again and again
 }
+
+
+// The code below is easier to read but works the same as the 4 lines above ...
+// ! While looping, if the elements in between the start and the end of the loop are checked, we skip them directly.
+// function checkElements() {
+//   if (inputsArr.indexOf(inputStart) < inputsArr.indexOf(inputEnd)) {
+//     for (let el = inputsArr.indexOf(inputStart); el !== inputsArr.indexOf(inputEnd); inputsArr.indexOf(inputStart) < inputsArr.indexOf(inputEnd) ? el++ : el--) {
+//       // console.log(inputsArr[el].checked)
+//       if (inputsArr[el].checked) continue // ? Skip inputs already checked
+//       inputsArr[el].checked = true
+//     }
+//   } else if ((inputsArr.indexOf(inputStart) > inputsArr.indexOf(inputEnd))) {
+//     for (let el = inputsArr.indexOf(inputStart); el !== inputsArr.indexOf(inputEnd); el--) {
+//       if (inputsArr[el].checked) continue // ? Skip inputs already checked
+//       inputsArr[el].checked = true
+//     }
+//   }
+//   clickCount = 0; // ? Reinitialise the click counter, so we can use the feature again and again
+// }
